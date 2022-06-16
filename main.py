395072a68d6ee8514b363e743f526f613ceb07d3
AtakTalay <talay.yucel@gmail.com>
2022-06-16 17:29:41 +0300

    Footer is added with some style changes

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ import locale
 #To print information neatly to the console
 from rich.console import Console
 from rich.table import Table
-
+from rich.style import Style
+from rich import box
 
 JSON_FILE_LOCATION = "commands.json"
 file = open(JSON_FILE_LOCATION)
@@ -40,20 +41,29 @@ lang = args.language
 logging.debug("Locale language is " + locale_lang + " and chosen language is: " + lang)
 
 #Table elements in turkish and english
-tableElements = {"title" : {"en": "TRUBA Commands", "tr": "TRUBA Komutlari"},
+tableElements = {"title" : {"en": "TRUBA Commands", "tr": "TRUBA Komutları"},
                  "firstCol": {"en": "Command", "tr": "Komut"},
-                 "secondCol": {"en": "Explanation", "tr": "Aciklama"}}
+                 "secondCol": {"en": "Explanation", "tr": "Açıklama"}}
+footerDesc = "Görüş ve önerileriniz için grid-teknik@ulakbim.gov.tr adresine mail atabilirsiniz"\
+if locale_lang == "tr" else "You can send mail to grid-teknik@ulakbim.gov.tr for you comments and suggestions"
 
+title_style = Style(bold=True,)
+col1_footer_style = Style(color="red", bold=False)
+col2_footer_style = Style(bold=False)
 #Creates table in the final language
-table = Table(title= tableElements["title"][lang], show_lines=True)
-table.add_column(tableElements["firstCol"][lang])
-table.add_column(tableElements["secondCol"][lang], justify="center")
+table = Table(title= tableElements["title"][lang], show_lines=True, show_footer=True, title_style= title_style)
+table.add_column(tableElements["firstCol"][lang] ,style="red",footer="Hint:",footer_style= col1_footer_style)
+table.add_column(tableElements["secondCol"][lang], footer=footerDesc, footer_style= col2_footer_style)
 
 #Adds commands with their explanations in final language
 for command in data:
     explanation = data[command][lang]
-    table.add_row(command, explanation)
+    table.add_row(command+":", explanation)
     logging.debug("Command " + command + " with explanation " + explanation)
+
+table.box = box.SIMPLE_HEAVY
+
+
 
 #Prints the result to the console
 console = Console()
