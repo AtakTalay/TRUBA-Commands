c1ae4fcd8d3546152080ee47d880fd5d1fe30416
AtakTalay <talay.yucel@gmail.com>
2022-06-20 14:39:10 +0300

    documentation and verbose argument is added

--- src/commands.py
+++ src/commands.py
@@ -21,12 +21,20 @@ from rich import box
 
 from pathlib import Path
 
+# To support multilingual text
 import gettext
+
+from time import perf_counter
 
 JSON_FILE_LOCATION = Path().resolve() / "commands.json"
 
 
 def get_commands():
+    """
+        Reads the commands from the JSON file and returns it as a dictionary
+    :return:
+        data (dict): Commands in respected language as a nested dictionary (data[command][language])
+    """
     with open(JSON_FILE_LOCATION, "r") as file:
         # reads json and writes into dictionary
         data = json.load(file)
@@ -34,9 +42,13 @@ def get_commands():
 
 
 def get_system_language():
-    """If the default environment's language is turkish set locales lang to turkish
-    and sets it to english otherwise
     """
+        If the default environment's language is turkish set locales lang to turkish
+        and sets it to english otherwise, then returns the language
+    :return:
+        locale_lang (str): locale language (en or tr)
+    """
+
     locale_lang = (locale.getdefaultlocale()[0] or "en-US")[:2]
     if locale_lang != "tr":
         locale_lang = "en"
@@ -44,6 +56,7 @@ def get_system_language():
 
 
 if __name__ == "__main__":
+    start = perf_counter()
     logging.basicConfig(filename=None, level=logging.ERROR)
 
     logger = logging.getLogger('main')
@@ -54,6 +67,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-l", "--language", nargs="?", type=str,choices=["tr", "en"],
                         default=system_lang, help="Selects the language")
+    parser.add_argument("-v", "--verbose",action="count", help = "Shows the running time")
 
     args = parser.parse_args()
     lang = args.language
@@ -62,7 +76,7 @@ if __name__ == "__main__":
     _ = translation.gettext
     logger.debug("Locale language is " + system_lang + " and chosen language is: " + lang)
 
-    footerMsg = _("You can send mail to grid-teknik@ulakbim.gov.tr for you comments and suggestions")
+    footerMsg = _("You can send mail to grid-teknik@ulakbim.gov.tr for you comments and suggestions.")
 
     # styles
     title_style = Style(bold=True)
@@ -87,3 +101,7 @@ if __name__ == "__main__":
     # Prints the result to the console
     console = Console()
     console.print(table)
+
+    end = perf_counter()
+    if args.verbose:
+        print("Time elapsed:", end - start)
