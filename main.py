03b3e0e489aae0b066805fd571f8366fc9677ec6
AtakTalay <talay.yucel@gmail.com>
2022-06-20 10:27:11 +0300

    gettext is implemented

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 import json
 
 # To write log messages
+import locales
 import logging
 
 # To offer optional arguments
+import os
+import pathlib
 from argparse import ArgumentParser
 
 # To get the language of the user's default environment
@@ -18,6 +21,8 @@ from rich import box
 
 from pathlib import Path
 
+import gettext
+
 JSON_FILE_LOCATION = Path().resolve() / "commands.json"
 
 
@@ -29,7 +34,7 @@ def get_commands():
 
 
 def get_system_language():
-    """If the default environment's language is turkish set locale lang to turkish
+    """If the default environment's language is turkish set locales lang to turkish
     and sets it to english otherwise
     """
     locale_lang = (locale.getdefaultlocale()[0] or "en-US")[:2]
@@ -44,21 +49,20 @@ if __name__ == "__main__":
     logger = logging.getLogger('main')
 
     system_lang = get_system_language()
+
     # gets the optional argument language to decide the final language to use
     parser = ArgumentParser()
-    parser.add_argument("-l", "--language", nargs="?", type=str, default=system_lang, help="Selects the language")
+    parser.add_argument("-l", "--language", nargs="?", type=str,choices=["tr", "en"],
+                        default=system_lang, help="Selects the language")
 
     args = parser.parse_args()
     lang = args.language
+    translation = gettext.translation('base', localedir='locales', languages=[args.language])
+    translation.install()
+    _ = translation.gettext
     logger.debug("Locale language is " + system_lang + " and chosen language is: " + lang)
 
-    # Table elements in turkish and english
-    tableElements = {"title": {"en": "TRUBA Commands", "tr": "TRUBA Komutları"},
-                     "firstCol": {"en": "Command", "tr": "Komut"},
-                     "secondCol": {"en": "Explanation", "tr": "Açıklama"}}
-
-    footerDesc = "Görüş ve önerileriniz için grid-teknik@ulakbim.gov.tr adresine mail atabilirsiniz" \
-        if lang == "tr" else "You can send mail to grid-teknik@ulakbim.gov.tr for you comments and suggestions"
+    footerMsg = _("You can send mail to grid-teknik@ulakbim.gov.tr for you comments and suggestions")
 
     # styles
     title_style = Style(bold=True)
@@ -66,9 +70,9 @@ if __name__ == "__main__":
     col2_footer_style = Style(bold=False)
 
     # Creates table in the final language
-    table = Table(title=tableElements["title"][lang], show_lines=True, show_footer=True, title_style=title_style)
-    table.add_column(tableElements["firstCol"][lang], style="red", footer="Hint:", footer_style=col1_footer_style)
-    table.add_column(tableElements["secondCol"][lang], footer=footerDesc, footer_style=col2_footer_style)
+    table = Table(title=_("TRUBA Commands"), show_lines=True, show_footer=True, title_style=title_style)
+    table.add_column(_("Command"), style="red", footer="Hint:", footer_style=col1_footer_style)
+    table.add_column(_("Explanation"), footer=footerMsg, footer_style=col2_footer_style)
 
     data = get_commands()
 
